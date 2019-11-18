4ac944a924562b76ab7d70514707e71ecc683fe8
Mshtarii Investments <malwandemadlala@gmail.com>
2019-11-18 13:54:06 +0200

    First django application

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Mshtarii Investments\Desktop\PycharmProjects\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
